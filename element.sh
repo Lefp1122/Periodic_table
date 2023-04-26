0f8bc929832af4940f1f440f0d7df4bab2015f85
Lefp1122 <luisfelix26@hotmail.com>
2023-04-26 22:45:51 +0000

    feat: add argument verification

--- element.sh
+++ element.sh
@@ -2,3 +2,11 @@
 
 PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 
+#Verify if there is an argument.
+if [ $# -eq 0 ]
+  then
+    echo "Please provide an element as an argument."
+
+    else
+    
+fi
