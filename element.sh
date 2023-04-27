75288bcf82e416cf96d76223e9393297f5606f5a
Lefp1122 <luisfelix26@hotmail.com>
2023-04-27 00:38:22 +0000

    feat: Add Symbol Verification

--- element.sh
+++ element.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 #Verify if there is an argument.
 if [ $# -eq 0 ]
@@ -8,5 +8,31 @@ if [ $# -eq 0 ]
     echo "Please provide an element as an argument."
 
     else
-    
+   
+  
+   #Verify if it is an Atomic number.
+    ATOMIC_NUMBER_VERIFY=$($PSQL "Select name from elements where atomic_number = $1 ")
+
+   #If not an Atomic NUmber
+    if [[ -z $ATOMIC_NUMBER_VERIFY ]]
+      then
+
+  
+      #Verify if it is a Symbol
+      SYMBOL_VERIFY=$($PSQL "Select name from elements where symbol = '$1' ")
+
+      echo $SYMBOL_VERIFY
+      #iF NOT A SYMBOL
+      if [[ -z $SYMBOL_VERIFY ]]
+      then
+      
+      echo " Not"
+
+      fi
+      
+
+
+    fi
+
+  
 fi
